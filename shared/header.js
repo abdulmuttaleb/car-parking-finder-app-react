f19ec27f05177f5cab0781c447d631e3a8f484c5
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-05-14 05:20:18 +0200

    styling header

--- shared/header.js
+++ shared/header.js
@@ -1,18 +1,38 @@
 import React from 'react'
-import { View, StyleSheet, Dimensions, Text } from 'react-native'
+import { View, StyleSheet, Dimensions, Text, TouchableWithoutFeedback } from 'react-native'
+import * as theme from '../themes/theme'
+import { Ionicons} from '@expo/vector-icons'
 
 export default function Header(){
     return(
         <View style={styles.header}>
-            <Text>Header</Text>
+            <View style={{ flex: 1, justifyContent: 'center'}}>
+                <Text style={styles.headerTitle}>Detected Location</Text>
+                <Text style={styles.headerLocation}>San Francisco, US</Text>    
+            </View>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems:'flex-end'}}>
+                <TouchableWithoutFeedback>
+                    <Ionicons name='ios-menu' size={theme.SIZES.icon * 1.5}/>
+                </TouchableWithoutFeedback>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     header:{
-        flex: 0.5,
+        flexDirection: 'row',
         justifyContent: 'center',
-        paddingHorizontal: 24
+        paddingHorizontal: theme.SIZES.base * 2,
+        paddingTop: theme.SIZES.base * 2.5,
+        paddingBottom: theme.SIZES.base * 1.5
+    },
+    headerTitle:{
+        color: theme.COLORS.gray
+    },
+    headerLocation:{
+        fontSize: theme.SIZES.font,
+        fontWeight: "500",
+        paddingVertical: theme.SIZES.base / 3
     }
 })
